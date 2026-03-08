3287960d86008d14d2fcef182277b21f842e65da
Kathurima Kimathi <kathurimakimathi415@gmail.com>
2026-03-08 14:18:11 +0300

    fix: logout
    Signed-off-by: Kathurima Kimathi <kathurimakimathi415@gmail.com>

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -1,8 +1,9 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { ACCESS_TOKEN } from "@/utilities/constants";
 
-export async function GET(req: Request) {
-  const response = NextResponse.redirect(new URL("/login", req.url));
+export async function GET(req: NextRequest) {
+  const loginUrl = new URL("/login", req.nextUrl.origin);
+  const response = NextResponse.redirect(loginUrl);
 
   // Clear the access token cookie by setting it with maxAge 0
   response.cookies.set({
