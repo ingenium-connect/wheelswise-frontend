66b4ea71e29223dde92913397b248cb5a3609120
Ingenium <Kathurimaeric415@outlook.com>
2026-05-14 23:45:30 +0300

    chore: update phone numbers (#147)
    Signed-off-by: Kathurima Kimathi <kathurimakimathi415@gmail.com>
    Co-authored-by: Kathurima Kimathi <kathurimakimathi415@gmail.com>

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -127,7 +127,7 @@ const Footer: React.FC = () => {
                   className="flex items-start gap-2.5 text-white/70 hover:text-white transition-colors"
                 >
                   <Phone className="w-4 h-4 shrink-0 mt-0.5 text-[#8bbfd8]" />
-                  +254 017 227 690
+                  0717 227 690 / 0735 620 339
                 </a>
               </li>
               <li>
