8769405b195e6f8375b04f19b3e5aa4732bf69b7
destiny-kay <destinykevogo@gmail.com>
2026-06-05 07:34:55 +0300

    fix: fix user flow one
    - fix user flow one from selecting a product

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,3 +1,4 @@
+import Signup from "@/components/auth/SignUp";
 import StandaloneSignup from "@/components/auth/standalone-signup/SignupForm";
 import FlowStepHeader from "@/components/layout/FlowStepHeader";
 import type { Metadata } from "next";
@@ -9,7 +10,13 @@ export const metadata: Metadata = {
   alternates: { canonical: "/signup" },
 };
 
-export default async function Page() {
+export default async function Page(searchParams: {
+  product_type?: string;
+  motor_type?: string;
+}) {
+  const params = await searchParams;
+  const motor_type = params?.motor_type || "PRIVATE";
+  const product_type = params.product_type || "COMPREHENSIVE";
   return (
     <>
       <FlowStepHeader
@@ -18,7 +25,11 @@ export default async function Page() {
       />
       <div className="bg-[#f0f6f9] flex-1 px-4 md:px-8 py-8">
         <div className="max-w-2xl mx-auto">
-          <StandaloneSignup />
+          {!motor_type && !product_type ? (
+            <StandaloneSignup />
+          ) : (
+            <Signup product_type={product_type} motor_type={motor_type} />
+          )}
         </div>
       </div>
     </>
